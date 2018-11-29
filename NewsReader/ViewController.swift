a0ea4d4d66199b72beac23490157511c7e63ac9e
Sinisa Vukovic <sinisa503@gmail.com>
2018-11-29 12:29:54 +0100

    Initial Commit

---
+++ NewsReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsReader
+//
+//  Created by Sinisa Vukovic on 29/11/2018.
+//  Copyright © 2018 Sinisa Vukovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+
+}
+
